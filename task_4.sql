8873087b40eac7e7f907d5e58498b1a340a32c2c
Brianer <brianerngeno@gmail.com>
2025-10-19 23:56:36 +0300

    Add task_4.sql with full description of Books table in alx_book_store

--- task_4.sql
+++ task_4.sql
@@ -1,6 +1,7 @@
--- Get full description of Books table
+-- Full description of the Books table in alx_book_store
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_NAME = 'Books'
-  AND TABLE_SCHEMA = DATABASE()
+  AND TABLE_SCHEMA = 'alx_book_store'
 ORDER BY ORDINAL_POSITION;
+``

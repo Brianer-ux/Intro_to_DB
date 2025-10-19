f4af939f546cb48d83c0f63c5eb66056e7b356a2
Brianer <brianerngeno@gmail.com>
2025-10-20 00:06:35 +0300

    Fix task_4.sql: select full description of books table from INFORMATION_SCHEMA with correct schema

--- task_4.sql
+++ task_4.sql
@@ -1,7 +1,5 @@
--- Full description of the Books table in alx_book_store
+-- Get full description of the books table in alx_book_store
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'Books'
-  AND TABLE_SCHEMA = 'alx_book_store'
-ORDER BY ORDINAL_POSITION;
-``
+WHERE TABLE_NAME = 'books'
+  AND TABLE_SCHEMA = 'alx_book_store';
